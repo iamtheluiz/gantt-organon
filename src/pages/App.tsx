74e02f07f6ce1cd0de7e30aab66fcef15d007ed7
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-03-18 15:48:30 -0300

    feat: set font config and manifest update

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+    >
+      <h1>Gantt Organon</h1>
+      <span>Project Schedule in WEB</span>
+    </div>
+  );
+}
+
+export default App;
