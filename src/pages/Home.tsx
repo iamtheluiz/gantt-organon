4e3822f1598fad0210634ebaf4b7898d758ac180
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-03-18 18:32:34 -0300

    feat: add initial Home design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,50 @@
+import { FiPlus } from 'react-icons/fi';
+import { BiRocket } from 'react-icons/bi';
+
+import '../styles/pages/Home.css';
+import { CSSProperties } from 'react';
+
 function Home() {
+  function handleOpenNewProjectModal() {
+
+  }
+
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }}
-    >
-      <h1>Gantt Organon</h1>
-      <span>Project Schedule in WEB</span>
-    </div>
+    <section id="home">
+      <header>
+        <h1>Gantt Organon</h1>
+        <span>Project Schedule in WEB</span>
+      </header>
+      <div className="content">
+        <ul>
+          <li>
+            <div
+              className="projectItem"
+              style={{ '--active-color': '#359756' } as CSSProperties}
+              onClick={handleOpenNewProjectModal}
+              onKeyDown={handleOpenNewProjectModal}
+              role="button"
+              tabIndex={0}
+            >
+              <div className="iconContainer">
+                <FiPlus />
+              </div>
+              <strong>Create Project</strong>
+              <span>New project schedule</span>
+            </div>
+          </li>
+          <li>
+            <div className="projectItem" style={{ '--active-color': '#843794' } as CSSProperties}>
+              <div className="iconContainer">
+                <BiRocket />
+              </div>
+              <strong>Rocket Project</strong>
+              <span>Launched rockets</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
