26f48a53aaf9061fdcb5e52045cf529eb84c7f91
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-06-05 13:24:31 -0300

    fix: sort Task by start date and day count

--- src/utils/sortTaskList.ts
+++ src/utils/sortTaskList.ts
@@ -1,7 +1,8 @@
 import { Task } from '../contexts/project';
+import getDayCount from './getDayCount';
 
 export default function sortTaskList(taskList: Task[]) {
-  taskList.sort((a, b) => a.start.getTime() - b.start.getTime());
+  taskList.sort((a, b) => a.start.getTime() - b.start.getTime() || getDayCount(a.start, a.end) - getDayCount(b.start, b.end));
 
   return taskList;
 }
