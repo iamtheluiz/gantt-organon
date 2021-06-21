db1f605a4e931e6c0e7b3a1869768afaf5a0ffe8
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-06-21 01:11:37 -0300

    feat: create service worker

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'gantt-organon-app';
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'CACHE_URLS') {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(event.data.payload)),
+    );
+  }
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }),
+  );
+});
---
+++ src/serviceWorker.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/prefer-default-export
+export const registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then((registration: any) => {
+        // Registration was successful
+        const data = {
+          type: 'CACHE_URLS',
+          payload: [
+            location.href,
+            ...performance.getEntriesByType('resource').map((r) => r.name),
+          ],
+        };
+
+        registration.installing.postMessage(data);
+      }).catch((err) => {
+        // registration failed :(
+        // console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+};
